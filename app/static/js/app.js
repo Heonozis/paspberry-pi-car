b3b692584d21076cbbcbe4431298df876b893f5c
Platon <heonozis4@gmail.com>
2018-03-23 21:19:59 +0200

    add joystick event handler

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,6 +1,13 @@
-var static = nipplejs.create({
+var joystick = nipplejs.create({
     zone: document.getElementById('static'),
     mode: 'static',
     position: {left: '50%', top: '50%'},
     color: 'red'
 });
+
+joystick.on('move', send_coordinates)
+
+function send_coordinates(event, data) {
+    console.log(event)
+    console.log(data)
+}
